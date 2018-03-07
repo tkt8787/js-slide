f85138790f370db1e60988fa086373ad05d69dfd
Minoru Takita <minoruTAKITA@MacBook.local>
2018-03-07 17:11:22 +0900

    モーダル処理

--- sample.js
+++ sample.js
@@ -1,89 +1,15 @@
-// function sample1() {
-//   var count = 1;
-//   console.log(count);
-// }
-// sample1();
+var modalBtn = document.getElementById('modal-btn');
+var modalWindow = document.getElementById('modal-window');
+var modalContent = document.getElementById('modal-content');
 
-// 2
-function sample2() {
+modalBtn.addEventListener('click' , function() {
+  modalWindow.classList.add('modal-open');
+});
 
-  console.log(count);
-  var count = 1;
-  return count;
-}
-// sample2();
-// console.log( sample2() );
+modalWindow.addEventListener('click' , function() {
+  modalWindow.classList.remove('modal-open');
+});
 
-// 3
-// function sample3() {
-//   var count = 1;
-//   return count;
-// }
-// var func = sample3();
-// console.log(func);
-// console.log( sample3() );
-
-function sample4() {
-  var count = 1;
-  return function() {
-    alert(count);
-    count++;
-  }
-}
-
-var func1 = sample4();
-var btn03 = document.getElementById('btn-03');
-btn03.addEventListener('click', func1);
-// console.log( sample4() );
-// console.log(func1());
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-// func1();
-
-console.log('');
-// var num = 1;
-
-function sample5() {
-  console.log(num);
-  num++
-  console.log(num);
-}
-// sample5();
-// sample5();
-// sample5();
-// sample5();
-// sample5();
-
-var btn01 = document.getElementById('btn-01');
-var btn02 = document.getElementById('btn-02');
-
-// btn01.addEventListener('click', click2);
-// btn02.addEventListener('click', click3);
-
-// var amount = 0;
-// function click() {
-//   console.log(amount);
-//   amount++;
-// }
-//
-// var amount1 = 0;
-// function click2() {
-//   console.log(amount1);
-//   amount1++;
-// }
-//
-// var amount2 = 0;
-// function click3() {
-//   console.log(amount2);
-//   amount2++;
-// }
-
-(function(a) {
-  var num = 1 + a;
-  console.log(num);
-})(10);
-
-// console.log(num);
+modalContent.addEventListener('click' , function(e) {
+  e.stopPropagation();
+});
